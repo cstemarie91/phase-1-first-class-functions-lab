83eeff3d4927b8a06171df738fecd9ecce959c88
+ <+>
2024-06-24 20:57:52 -0400

    Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    
+    
+    function returnFirstTwoDrivers() {
+        return drivers.slice(0, 2)
+
+    }
+    function returnLastTwoDrivers() {
+        return drivers.slice(2, 4)
+    }
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    function createFareMultiplier() {
+        return function fareMultiplier() {
+            return 5 * 5;
+        }; 
+    }
+    function fareDoubler(fare) {
+        return fare * 2;
+    }
+    
+    function fareTripler(fare) {
+        return fare * 3;
+    }
+        
+    
+    function selectDifferentDrivers(drivers, functions) {
+        if(functions === returnFirstTwoDrivers === true)
+            return drivers.slice(0, 2)
+        else if(functions === returnLastTwoDrivers === true)
+            return drivers.slice(2, 4)
+    }
+
